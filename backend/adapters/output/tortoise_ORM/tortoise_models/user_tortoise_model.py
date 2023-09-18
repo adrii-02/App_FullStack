9dca4bcde6c7f267998b11018a7addd8a1b5dbcc
capy <garcia.lopez.adrian@alumnat.copernic.cat>
2023-09-18 13:01:51 +0200

    Implementación de requirements.txt

--- backend/adapters/output/tortoise_ORM/tortoise_models/user_tortoise_model.py
+++ backend/adapters/output/tortoise_ORM/tortoise_models/user_tortoise_model.py
@@ -3,14 +3,13 @@ from tortoise import fields
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 class User(Model):
-    #color= fields.CharField(maz_digits=4, decimal_place=2, nullable=False)
     id= fields.IntField(pk=True)
     email = fields.CharField(max_length=100, nullable=False)
     name = fields.CharField(max_length=30, nullable=False)
     age = fields.IntField(nullable=False)
     length = fields.DecimalField(max_digits=4, decimal_places=2, nullable=False)
     weight = fields.DecimalField(max_digits=3, decimal_places=2, nullable=False)
-#language= field.DecimalField(max_digits=3,decimal_places=2, nullable=False, puquemi_bb=ositu, mibb_aquechi?;mibebeeeeee)
+
     #Foreign Key
     #role = fields.ForeignKeyField('models.Role', related_name="roles")
 
